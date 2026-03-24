0a4494bb42e0e95d175aa008c024b9a416c30d5c
ParkJinwon1025 <jinwon2812@ubisam.com>
2026-03-24 17:57:13 +0900

    임시 커밋

--- src/lib/Subscriber.hpp
+++ src/lib/Subscriber.hpp
@@ -10,8 +10,5 @@ public:
     Subscriber() = default;
     virtual ~Subscriber() = default;
 
-    void dispatch(const std::string &body, Session &session); // 내부 라우터 (오버라이드 불필요)
-
-    virtual void receive(const std::string &body, Session &session);      // 문자열이 왔을 때
-    virtual void receive(const nlohmann::json &json, Session &session);   // JSON이 왔을 때
+    virtual void receive(Session &session, const nlohmann::json &json); // JSON이 왔을 때
 };
